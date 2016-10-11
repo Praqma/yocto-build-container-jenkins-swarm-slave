da67510dcc9986fd29eba009f19934113a8b6f9f
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2016-10-11 13:19:33 +0200

    Print command that gets executed at the slave start
    Signed-off-by: Andrey Devyatkin <andrey.a.devyatkin@gmail.com>

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 JAR=`ls -1 swarm-client-*.jar | tail -n 1`
-  
-exec java ${JAVA_OPTS} -jar ${JAR} "$@"
+CMD="java ${JAVA_OPTS} -jar ${JAR} $@"
+echo "Run: ${CMD}"
+exec ${CMD}
